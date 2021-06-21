e6d2a991fbf9b197d66eda91ff5697d4c34fc0e3
Jérémy Lourenço <jerem.lourenco@gmail.com>
2021-06-21 01:48:50 +0100

    [xtime] Add license file headers (#5)

--- xtime/time.go
+++ xtime/time.go
@@ -1,3 +1,8 @@
+// Copyright 2021 Jérémy Lourenço. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package xtime provides additional primitives and structures for measuring and displaying time.
 package xtime
 
 import (
