9f93a06994daf7240cb92129494fa9430cb63a07
Jérémy Lourenço <jeremy@lourenco.io>
2021-07-10 13:01:54 +0100

    [xhttp] ReplaceHeader - handle duplicate prefixed keys

--- xnet/xhttp/example_test.go
+++ xnet/xhttp/example_test.go
@@ -12,6 +12,17 @@ import (
 	"github.com/jlourenc/xgo/xnet/xhttp"
 )
 
+func ExampleHeaderExist() {
+	headers := http.Header{
+		"Header-Key": {"key1=val1", "key2", "key3=val3, key4"},
+	}
+
+	exist := xhttp.HeaderExist(headers, "Header-Key")
+
+	fmt.Printf("got: %t", exist)
+	// Output: got: true
+}
+
 func ExampleHeaderKeyValues() {
 	headers := http.Header{
 		"Header-Key": {"key1=val1", "key2", "key3=val3, key4"},
@@ -54,7 +65,12 @@ func ExampleReplaceHeader() {
 	}
 
 	xhttp.ReplaceHeader(headers, "prefix", "header-key", "key5=val5", "key6")
+	fmt.Printf("got: %s\n", headers)
 
-	fmt.Printf("got: %s", headers)
-	// Output: got: map[Header-Key:[key5=val5 key6] Prefix-Header-Key:[key1=val1 key2 key3=val3, key4]]
+	xhttp.ReplaceHeader(headers, "prefix", "header-key", "key7=val7", "key8")
+	fmt.Printf("got: %s\n", headers)
+
+	// Output:
+	// got: map[Header-Key:[key5=val5 key6] Prefix-Header-Key:[key1=val1 key2 key3=val3, key4]]
+	// got: map[Header-Key:[key7=val7 key8] Prefix-1-Header-Key:[key1=val1 key2 key3=val3, key4] Prefix-Header-Key:[key5=val5 key6]]
 }
