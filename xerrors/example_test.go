d789c35faaccd8b65629bd7d8b74ce4ec75e2821
Jérémy Lourenço <jeremy@lourenco.io>
2021-07-18 22:31:44 +0100

    [xerrors] Add documentation

--- xerrors/example_test.go
+++ xerrors/example_test.go
@@ -6,15 +6,42 @@ package xerrors_test
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jlourenc/xgo/xerrors"
 )
+
+func ExampleAs() {
+	if _, err := os.Open("non-existing"); err != nil {
+		var pathError *os.PathError
+		if xerrors.As(err, &pathError) {
+			fmt.Println("Failed at path:", pathError.Path)
+		} else {
+			fmt.Println(err)
+		}
+	}
+
+	// Output: Failed at path: non-existing
+}
+
+func ExampleIs() {
+	if _, err := os.Open("non-existing"); err != nil {
+		if xerrors.Is(err, os.ErrNotExist) {
+			fmt.Println("file does not exist")
+		} else {
+			fmt.Println(err)
+		}
+	}
+
+	// Output: file does not exist
+}
 
 func ExampleNew() {
 	err := xerrors.New("emit macho dwarf: elf header corrupted")
 	if err != nil {
 		fmt.Print(err)
 	}
+
 	// Output: emit macho dwarf: elf header corrupted
 }
 
@@ -24,5 +51,36 @@ func ExampleNewf() {
 	if err != nil {
 		fmt.Print(err)
 	}
+
 	// Output: user "bimmler" (id 17) not found
+}
+
+func ExampleUnwrap() {
+	err := xerrors.New("elf header corrupted")
+	err = xerrors.Wrap(err, "emit macho dwarf")
+	err = xerrors.Unwrap(err)
+	fmt.Println(err)
+
+	// Output: elf header corrupted
+}
+
+func ExampleWithStack() {
+	err := xerrors.WithStack(os.ErrNotExist)
+	fmt.Println(err)
+
+	// Output: file does not exist
+}
+
+func ExampleWrap() {
+	err := xerrors.Wrap(os.ErrNotExist, "failed to open file")
+	fmt.Println(err)
+
+	// Output: failed to open file: file does not exist
+}
+
+func ExampleWrapf() {
+	err := xerrors.Wrapf(os.ErrNotExist, "failed to open file %s", "myfile")
+	fmt.Println(err)
+
+	// Output: failed to open file myfile: file does not exist
 }
